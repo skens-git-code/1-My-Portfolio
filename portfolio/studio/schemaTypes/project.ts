2025c7d79a46ea1bde25544ae6d375ef76e561d4
Sarthak Mathapati <skens.code@gmail.com>
2026-09-08 17:19:10 +0530

    hj

---
+++ portfolio/studio/schemaTypes/project.ts
@@ -0,0 +1,29 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title' },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      title: 'Project Image',
+      type: 'image',
+      options: { hotspot: true },
+    },
+  ],
+}
